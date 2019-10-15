5175cc3ba2e9847e2c668d08634cf6412b591c40
BartArys <mysterybtdn@gmail.com>
2019-10-15 23:59:10 +0200

    Add tests for text arguments

--- kordx-commands-runtime/src/main/kotlin/com/gitlab/kordlib/kordx/commands/argument/text/Extensions.kt
+++ kordx-commands-runtime/src/main/kotlin/com/gitlab/kordlib/kordx/commands/argument/text/Extensions.kt
@@ -6,19 +6,19 @@ import com.gitlab.kordlib.kordx.commands.argument.Result
 import com.gitlab.kordlib.kordx.commands.argument.filter
 
 fun <CONTEXT> Argument<String, CONTEXT>.whitelist(
-        vararg words: String, ignoreCase: Boolean = false
+        vararg whitelist: String, ignoreCase: Boolean = false
 ): Argument<String, CONTEXT> = object : Argument<String, CONTEXT> by this {
     override val example: String
-        get() = words.random()
+        get() = whitelist.random()
 
     override suspend fun parse(words: List<String>, fromIndex: Int, context: CONTEXT): Result<String> {
         return this@whitelist.parse(words, fromIndex, context).filter {
             when {
                 ignoreCase -> when {
-                    words.any { word -> word.equals(it, true) } -> FilterResult.Pass
+                    whitelist.any { word -> word.equals(it, true) } -> FilterResult.Pass
                     else -> FilterResult.Fail("expected one of $words (not case sensitive) but got $it")
                 }
-                it in words -> FilterResult.Pass
+                it in whitelist -> FilterResult.Pass
                 else -> FilterResult.Fail("expected one of $words (case sensitive) but got $it")
             }
         }
---
+++ kordx-commands-runtime/src/test/kotlin/com/gitlab/kordlib/kordx/commands/argument/text/WhiteListTest.kt
@@ -0,0 +1,38 @@
+package com.gitlab.kordlib.kordx.commands.argument.text
+
+import com.gitlab.kordlib.kordx.commands.argument.primitives.IntArgument
+import com.gitlab.kordlib.kordx.commands.argument.primitives.inRange
+import com.gitlab.kordlib.kordx.commands.argument.requireFailure
+import com.gitlab.kordlib.kordx.commands.argument.requireItem
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+class WhiteListTest {
+
+    val argument = WordArgument.whitelist("whitelist")
+
+    @ParameterizedTest
+    @MethodSource("passingSources")
+    fun `correctly accepts arguments`(text: String) = runBlockingTest {
+        argument.parse(listOf(text), 0, Unit).requireItem(text)
+    }
+
+    @ParameterizedTest
+    @MethodSource("failingSources")
+    fun `correctly fails arguments`(text: String) = runBlockingTest {
+        argument.parse(listOf(text), 0, Unit).requireFailure()
+    }
+
+
+    companion object {
+        val range = -5..5
+
+        @JvmStatic
+        fun passingSources() = listOf(Arguments.of("whitelist"))
+
+        @JvmStatic
+        fun failingSources() = listOf(Arguments.of("false"))
+    }
+}
