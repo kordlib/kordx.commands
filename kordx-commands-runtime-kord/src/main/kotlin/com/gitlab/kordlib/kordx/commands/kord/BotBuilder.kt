a9a53301bf0e9be5c1ff48f719a5dd4608af30e1
BartArys <mysterybtdn@gmail.com>
2019-12-27 16:28:56 +0100

    Update BotBuilder to 0.3.0

--- kordx-commands-runtime-kord/src/main/kotlin/com/gitlab/kordlib/kordx/commands/kord/BotBuilder.kt
+++ kordx-commands-runtime-kord/src/main/kotlin/com/gitlab/kordlib/kordx/commands/kord/BotBuilder.kt
@@ -1,15 +1,15 @@
 package com.gitlab.kordlib.kordx.commands.kord
 
-import com.gitlab.kordlib.core.builder.kord.KordClientBuilder
+import com.gitlab.kordlib.core.builder.kord.KordBuilder
 import com.gitlab.kordlib.kordx.commands.kord.context.KordEventSource
 import com.gitlab.kordlib.kordx.commands.pipe.PipeConfig
 import com.gitlab.kordlib.kordx.commands.pipe.Prefix
 
 class BotBuilder(token: String) {
-    val kordBuilder: KordClientBuilder = KordClientBuilder(token)
+    val kordBuilder: KordBuilder = KordBuilder(token)
     val pipeConfig: PipeConfig = PipeConfig()
 
-    inline fun kord(builder: KordClientBuilder.() -> Unit) {
+    inline fun kord(builder: KordBuilder.() -> Unit) {
         kordBuilder.apply(builder)
     }
 
