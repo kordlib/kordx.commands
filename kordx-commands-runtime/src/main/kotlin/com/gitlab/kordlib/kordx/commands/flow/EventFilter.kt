6082c19dcd434a54e661d7fb7f7e32203df5418a
BartArys <mysterybtdn@gmail.com>
2019-12-27 16:27:23 +0100

    Make eventfilters suspend

--- kordx-commands-runtime/src/main/kotlin/com/gitlab/kordlib/kordx/commands/flow/EventFilter.kt
+++ kordx-commands-runtime/src/main/kotlin/com/gitlab/kordlib/kordx/commands/flow/EventFilter.kt
@@ -7,7 +7,7 @@ interface EventFilter<SOURCECONTEXT> {
     suspend operator fun invoke(event: SOURCECONTEXT): Boolean
 }
 
-fun <SOURCECONTEXT> eventFilter(context: CommandContext<SOURCECONTEXT, *, *>, filter: SOURCECONTEXT.() -> Boolean) = object : EventFilter<SOURCECONTEXT> {
+fun <SOURCECONTEXT> eventFilter(context: CommandContext<SOURCECONTEXT, *, *>, filter: suspend SOURCECONTEXT.() -> Boolean) = object : EventFilter<SOURCECONTEXT> {
     override val context: CommandContext<SOURCECONTEXT, *, *> = context
 
     override suspend fun invoke(event: SOURCECONTEXT): Boolean = filter(event)
