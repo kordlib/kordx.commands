d64d2d44ded445e25cc84112fbe1bdefe41bbc9f
BartArys <mysterybtdn@gmail.com>
2020-01-03 11:33:37 +0100

    Add more result mapping for nullable results

--- kordx-commands-runtime/src/main/kotlin/com/gitlab/kordlib/kordx/commands/argument/Result.kt
+++ kordx-commands-runtime/src/main/kotlin/com/gitlab/kordlib/kordx/commands/argument/Result.kt
@@ -1,7 +1,7 @@
 package com.gitlab.kordlib.kordx.commands.argument
 
-import com.gitlab.kordlib.kordx.commands.argument.FilterResult.Fail
-import com.gitlab.kordlib.kordx.commands.argument.FilterResult.Pass
+import com.gitlab.kordlib.kordx.commands.argument.Result.Failure
+import com.gitlab.kordlib.kordx.commands.argument.Result.Success
 
 /**
  * The product of an [Argument]'s parsing. A [Result] can either be a [Success] or a [Failure]
@@ -50,7 +50,12 @@ sealed class FilterResult {
         companion object
     }
 
-    companion object
+    companion object {
+        inline operator fun invoke(predicate: Boolean, failureReason: () -> String) = when {
+            predicate -> Pass
+            else -> Fail(failureReason())
+        }
+    }
 }
 
 /**
@@ -72,7 +77,19 @@ sealed class MapResult<T> {
         companion object
     }
 
-    companion object
+    companion object {
+
+        inline fun <reified R> filterInstance(item: Any, failMessage: String): MapResult<R> = when (item) {
+            is R -> Pass(item)
+            else -> Fail(failMessage)
+        }
+
+        inline fun <reified R> filterInstance(item: Any, failMessage: () -> String): MapResult<R> = when (item) {
+            is R -> Pass(item)
+            else -> Fail(failMessage())
+        }
+
+    }
 }
 
 /**
@@ -81,8 +98,8 @@ sealed class MapResult<T> {
  */
 @Suppress("UNCHECKED_CAST")
 inline fun <T, R> Result<T>.map(mapper: (T) -> R): Result<R> = when (this) {
-    is Result.Success -> Result.Success(mapper(item), wordsTaken)
-    is Result.Failure -> this as Result<R>
+    is Success -> Success(mapper(item), wordsTaken)
+    is Failure -> this as Result<R>
 }
 
 /**
@@ -90,8 +107,8 @@ inline fun <T, R> Result<T>.map(mapper: (T) -> R): Result<R> = when (this) {
  */
 @Suppress("UNCHECKED_CAST")
 inline fun <T> Result<T>.switchOnFail(generator: () -> Result<T>): Result<T> = when (this) {
-    is Result.Success -> this
-    is Result.Failure -> generator()
+    is Success -> this
+    is Failure -> generator()
 }
 
 /**
@@ -100,11 +117,11 @@ inline fun <T> Result<T>.switchOnFail(generator: () -> Result<T>): Result<T> = w
  */
 @Suppress("UNCHECKED_CAST")
 inline fun <T, R : Any> Result<T>.tryMap(mapper: (T) -> MapResult<R>): Result<R> = when (this) {
-    is Result.Success -> when (val result = mapper(item)) {
-        is MapResult.Pass -> Result.Success(result.item, wordsTaken)
-        is MapResult.Fail -> Result.Failure(result.reason, 0)
+    is Success -> when (val result = mapper(item)) {
+        is MapResult.Pass -> Success(result.item, wordsTaken)
+        is MapResult.Fail -> Failure(result.reason, 0)
     }
-    is Result.Failure -> this as Result<R>
+    is Failure -> this as Result<R>
 }
 
 /**
@@ -113,11 +130,41 @@ inline fun <T, R : Any> Result<T>.tryMap(mapper: (T) -> MapResult<R>): Result<R>
  */
 @Suppress("UNCHECKED_CAST")
 inline fun <T> Result<T>.filter(filter: (T) -> FilterResult): Result<T> = when (this) {
-    is Result.Success -> when (val result = filter(item)) {
+    is Success -> when (val result = filter(item)) {
         FilterResult.Pass -> this
-        is FilterResult.Fail -> Result.Failure(result.reason, 0)
+        is FilterResult.Fail -> Failure(result.reason, 0)
     }
-    is Result.Failure -> this
+    is Failure -> this
+}
+
+/**
+ * Returns a [Result.Success] if this result is a [Result.Success] *and* the [Result.Success.item] is [T],
+ * or a [Result.Failure] otherwise.
+ *
+ * @param [failMessage] the message to supply when the [Result.Success.item] is not [T].
+ */
+@Suppress("UNCHECKED_CAST")
+inline fun <reified T> Result<*>.filterIsInstance(failMessage: String): Result<T> = when (val result = this) {
+    is Success -> when (result.item) {
+        is T -> result as Result<T>
+        else -> Failure(failMessage, 0)
+    }
+    is Failure -> this as Result<T>
+}
+
+/**
+ * Returns a [Result.Success] if this result is a [Result.Success] *and* the [Result.Success.item] is [T],
+ * or a [Result.Failure] otherwise.
+ *
+ * @param [failMessage] the message to supply when the [Result.Success.item] is not [T].
+ */
+@Suppress("UNCHECKED_CAST")
+inline fun <reified T> Result<*>.filterIsInstance(failMessage: () -> String): Result<T> = when (val result = this) {
+    is Success -> when (result.item) {
+        is T -> result as Result<T>
+        else -> Failure(failMessage(), 0)
+    }
+    is Failure -> this as Result<T>
 }
 
 /**
@@ -128,33 +175,62 @@ inline fun <T> Result<T>.filter(filter: (T) -> FilterResult): Result<T> = when (
  */
 @Suppress("UNCHECKED_CAST")
 fun <T : Any> Result<T?>.filterNotNull(failMessage: String): Result<T> = when (this) {
-    is Result.Success -> when (item) {
-        null -> Result.Failure(failMessage, wordsTaken)
-        else -> Result.Success(item, wordsTaken)
+    is Success -> when (item) {
+        null -> Failure(failMessage, wordsTaken)
+        else -> Success(item, wordsTaken)
     }
-    is Result.Failure -> this as Result<T>
+    is Failure -> this as Result<T>
+}
+
+/**
+ * Returns a [Result.Success] if this result is a [Result.Success] *and* the [Result.Success.item] is not null,
+ * or a [Result.Failure] otherwise.
+ *
+ * @param [failMessage] the message to supply when the [Result.Success.item] is null.
+ */
+@Suppress("UNCHECKED_CAST")
+inline fun <T : Any> Result<T?>.filterNotNull(failMessage: (T?) -> String): Result<T> = when (this) {
+    is Success -> when (item) {
+        null -> Failure(failMessage(item), wordsTaken)
+        else -> Success(item, wordsTaken)
+    }
+    is Failure -> this as Result<T>
 }
 
 /**
  * Returns this if this is a [Result.Success] and [Result.Success.item] is not null, or a [Result.Success] with
- * the [fallBack] as item otherwise.
+ * the [default] as item otherwise.
  */
 @Suppress("UNCHECKED_CAST")
-fun <T : Any> Result<T?>.orDefault(fallBack: T): Result.Success<T> = when (this) {
-    is Result.Success -> when (item) {
-        null -> Result.Success(fallBack, wordsTaken)
+fun <T : Any> Result<T?>.orDefault(default: T): Result.Success<T> = when (this) {
+    is Success -> when (item) {
+        null -> Success(default, wordsTaken)
         else -> this as Result.Success<T>
     }
-    is Result.Failure -> Result.Success(fallBack, 0)
+    is Failure -> Success(default, 0)
 }
+
+/**
+ * Returns this if this is a [Result.Success] and [Result.Success.item] is not null, or a [Result.Success] with
+ * the [fallback] as item otherwise.
+ */
+@Suppress("UNCHECKED_CAST")
+inline fun <T : Any> Result<T?>.orSupplyDefault(fallback: () -> T): Result.Success<T> = when (this) {
+    is Success -> when (item) {
+        null -> Success(fallback(), wordsTaken)
+        else -> this as Result.Success<T>
+    }
+    is Failure -> Success(fallback(), 0)
+}
+
 
 /**
  * Returns this if this is a [Result.Success], or a [Result.Success] with the [fallBack] as item otherwise.
  */
 @Suppress("UNCHECKED_CAST")
 fun <T> Result<T>.orElse(fallBack: T): Result.Success<T> = when (this) {
-    is Result.Success -> Result.Success(item, wordsTaken)
-    is Result.Failure -> Result.Success(fallBack, 0)
+    is Success -> Success(item, wordsTaken)
+    is Failure -> Success(fallBack, 0)
 }
 
 /**
@@ -162,8 +238,8 @@ fun <T> Result<T>.orElse(fallBack: T): Result.Success<T> = when (this) {
  */
 @Suppress("UNCHECKED_CAST")
 inline fun <T> Result<T>.orElse(fallBack: () -> T): Result.Success<T> = when (this) {
-    is Result.Success -> Result.Success(item, wordsTaken)
-    is Result.Failure -> Result.Success(fallBack(), 0)
+    is Success -> Success(item, wordsTaken)
+    is Failure -> Success(fallBack(), 0)
 }
 
 /**
@@ -171,6 +247,6 @@ inline fun <T> Result<T>.orElse(fallBack: () -> T): Result.Success<T> = when (th
  */
 @Suppress("UNCHECKED_CAST")
 fun <T : Any> Result<T>.optional(): Result.Success<T?> = when (this) {
-    is Result.Success -> this as Result.Success<T?>
-    is Result.Failure -> Result.Success(null, 0)
+    is Success -> this as Result.Success<T?>
+    is Failure -> Success(null, 0)
 }
