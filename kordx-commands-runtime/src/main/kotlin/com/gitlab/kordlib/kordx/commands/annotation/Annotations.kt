7d78bf5aa51e7211f2511055d72ef56d2ae56255
BartArys <mysterybtdn@gmail.com>
2020-04-24 21:35:16 +0200

    Update documentation references

--- kordx-commands-runtime/src/main/kotlin/com/gitlab/kordlib/kordx/commands/annotation/Annotations.kt
+++ kordx-commands-runtime/src/main/kotlin/com/gitlab/kordlib/kordx/commands/annotation/Annotations.kt
@@ -6,13 +6,14 @@ import com.gitlab.kordlib.kordx.commands.model.processor.ProcessorConfig
  * Will mark this element for automatic registry to the [ProcessorConfig] using the annotation processor, suspending functions are supported.
  * Functions and properties annotated with [AutoWired] need to return one of the following types:
  * * [org.koin.core.module.Module]
- * * [com.gitlab.kordlib.kordx.commands.flow.ModuleModifier]
- * * [com.gitlab.kordlib.kordx.commands.processor.EventSource]
- * * [com.gitlab.kordlib.kordx.commands.processor.EventHandler]
- * * [com.gitlab.kordlib.kordx.commands.flow.EventFilter]
- * * [com.gitlab.kordlib.kordx.commands.flow.Precondition]
- * * [com.gitlab.kordlib.kordx.commands.model.CommandSet] (requires [ModuleName])
- * * [com.gitlab.kordlib.kordx.commands.processor.PrefixConfiguration]
+ * * [com.gitlab.kordlib.kordx.commands.model.module.ModuleModifier]
+ * * [com.gitlab.kordlib.kordx.commands.model.processor.EventSource]
+ * * [com.gitlab.kordlib.kordx.commands.model.processor.EventHandler]
+ * * [com.gitlab.kordlib.kordx.commands.model.eventFilter.EventFilter]
+ * * [com.gitlab.kordlib.kordx.commands.model.precondition.Precondition]
+ * * [com.gitlab.kordlib.kordx.commands.model.module.CommandSet] (requires [ModuleName])
+ * * [com.gitlab.kordlib.kordx.commands.model.prefix.PrefixConfiguration]
+ * * [com.gitlab.kordlib.kordx.commands.model.plug.Plug]
  *
  * > The target can also be applied to files, in which case the annotation processor will act as if every
  * > function or property with the above mentioned return types would be annotated with [AutoWired]
