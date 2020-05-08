933eec8381ae5f416e9cf021e5b0dc1ab4ab1905
BartArys <mysterybtdn@gmail.com>
2020-05-08 14:45:29 +0200

    Make InternalIntArgument actually internal

--- kordx-commands-runtime/src/main/kotlin/com/gitlab/kordlib/kordx/commands/argument/primitive/IntArgument.kt
+++ kordx-commands-runtime/src/main/kotlin/com/gitlab/kordlib/kordx/commands/argument/primitive/IntArgument.kt
@@ -5,7 +5,7 @@ import com.gitlab.kordlib.kordx.commands.argument.result.ArgumentResult
 import com.gitlab.kordlib.kordx.commands.argument.SingleWordArgument
 import kotlin.random.Random
 
-class InternalIntArgument(override val name: String = "Number") : SingleWordArgument<Int, Any?>() {
+internal class InternalIntArgument(override val name: String = "Number") : SingleWordArgument<Int, Any?>() {
     override val example: String
         get() = Random.nextInt(-100, 100).toString()
 
@@ -25,4 +25,4 @@ val IntArgument: Argument<Int, Any?> = InternalIntArgument()
  * Argument that matches against a single world, emitting success when the word is a valid integer value.
  */
 @Suppress("FunctionName")
-fun IntArgument(name: String) = InternalIntArgument(name)
+fun IntArgument(name: String): Argument<Int, Any?> = InternalIntArgument(name)
