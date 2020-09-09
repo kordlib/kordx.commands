fb2a543888d0134445fabf288a9db98a5ce359a0
Michael Rittmeister <michael@rittmeister.in>
2020-09-09 20:47:27 +0300

    Properly display contents of whitelist in help message

--- kordx-commands-runtime/src/main/kotlin/com/gitlab/kordlib/kordx/commands/argument/text/Extensions.kt
+++ kordx-commands-runtime/src/main/kotlin/com/gitlab/kordlib/kordx/commands/argument/text/Extensions.kt
@@ -4,6 +4,7 @@ import com.gitlab.kordlib.kordx.commands.argument.Argument
 import com.gitlab.kordlib.kordx.commands.argument.result.ArgumentResult
 import com.gitlab.kordlib.kordx.commands.argument.result.extension.FilterResult
 import com.gitlab.kordlib.kordx.commands.argument.result.extension.filter
+import java.util.*
 
 /**
  * Returns an Argument that, on top of the supplied argument, only accepts values in [whitelist].
@@ -13,15 +14,16 @@ import com.gitlab.kordlib.kordx.commands.argument.result.extension.filter
 fun <CONTEXT> Argument<String, CONTEXT>.whitelist(
         vararg whitelist: String, ignoreCase: Boolean = true
 ): Argument<String, CONTEXT> = object : Argument<String, CONTEXT> by this {
+    private val options = whitelist.contentToString()
     override suspend fun parse(text: String, fromIndex: Int, context: CONTEXT): ArgumentResult<String> {
         return this@whitelist.parse(text, fromIndex, context).filter(fromIndex) {
             when {
                 ignoreCase -> when {
                     whitelist.any { word -> word.equals(it, true) } -> FilterResult.Pass
-                    else -> FilterResult.Fail("expected one of $whitelist (not case sensitive) but got $it")
+                    else -> FilterResult.Fail("expected one of $options (not case sensitive) but got $it")
                 }
                 it in whitelist -> FilterResult.Pass
-                else -> FilterResult.Fail("expected one of $whitelist (case sensitive) but got $it")
+                else -> FilterResult.Fail("expected one of $options (case sensitive) but got $it")
             }
         }
     }
