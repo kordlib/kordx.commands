f234ba2c9826ff99c3129babf630a7942a9e15b9
Hope <34831095+HopeBaron@users.noreply.github.com>
2021-01-17 20:11:55 +0200

    apply Maven publishing (#31)
    * apply Maven publishing
    
    * Simplify module names
    
    * Correct module names variables
    
    * Apply Kotlin package styling

--- buildSrc/src/main/kotlin/Project.kt
+++ buildSrc/src/main/kotlin/Project.kt
@@ -1,12 +1,35 @@
 import org.gradle.kotlin.dsl.DependencyHandlerScope
 import org.gradle.kotlin.dsl.project
 
-val DependencyHandlerScope.processor get() = project(":kordx-commands-processor")
-val DependencyHandlerScope.runtime get() = project(":kordx-commands-runtime")
-val DependencyHandlerScope.`runtime-kord` get() = project(":kordx-commands-runtime-kord")
+val DependencyHandlerScope.processor get() = project(":processor")
+val DependencyHandlerScope.runtime get() = project(":runtime")
+val DependencyHandlerScope.`runtime-kord` get() = project(":runtime-kord")
+
+
+/**
+ * whether the process has been invoked by JitPack
+ */
+val isJitPack get() = "true" == System.getenv("JITPACK")
+
+
 
 object Library {
+    private const val releaseVersion = "0.4.0-SNAPSHOT"
+    val isSnapshot: Boolean get() = releaseVersion.endsWith("-SNAPSHOT")
+    val isRelease: Boolean get() = !isSnapshot
+    const val name = "commands"
     const val group = "dev.kord.x"
-    val version = System.getenv("RELEASE_TAG") ?: System.getenv("GITHUB_SHA") ?: "undefined"
+    val version: String = if (isJitPack) System.getenv("RELEASE_TAG")
+    else releaseVersion
+    const val url = "https://github.com/kordlib/kordx.commands"
+
     const val description = "Kotlin Command library for Kord and other APIs"
+    const val projectUrl = "https://github.com/kordlib/kordx.commands"
+
+    val isStableApi: Boolean get() = !isSnapshot
+}
+
+object Repo {
+    const val releasesUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+    const val snapshotsUrl = "https://oss.sonatype.org/content/repositories/snapshots/"
 }
